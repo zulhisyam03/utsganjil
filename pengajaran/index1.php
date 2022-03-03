d05b53b9105fe4774d244fd62a5f23e6df76d843
zulhisyam03 <zul0342@gmail.com>
2022-03-03 14:44:36 +0800

    index1

---
+++ pengajaran/index1.php
@@ -0,0 +1,27 @@
+<?php
+  print("Hari Ini Adalah Tanggal ".date("d F Y"));
+  print("<br>");
+  printf("Tanggal Hari Ini Adalah %s <br>",date("d F Y"));
+  echo "Hari Ini Adalah Tanggal ".date("d F Y");
+  echo "<p>";
+
+  define("SALAM","HALO APA KABAR"); //Konstanta
+  $nama = "Zul Hisyam"; //Variabel
+
+  echo SALAM." Nama Saya Adalah ".$nama;
+  printf("Tanggal Hari Ini Adalah %s %s <br>",date("d F Y"), $nama);
+  echo "Hello", "Zul Hisyam <p>";
+
+  $gaji = "500000 Rupiah";
+  $gajib = (int)$gaji;
+  $gajid = (double)$gaji;
+
+  printf("Gaji String : %s <br>",$gaji) ;
+  printf("Gaji Integer : %s <br>",$gajib) ;
+  printf("Gaji Double : %s <p>",$gajid) ;
+
+  printf ("1 + 5 = %d <br>", 1+5);
+  print ("2 * 5 = "); print(2*5);
+  print("<br>");
+  echo "10 / 2 = ",10/2,"<br>";
+?>
