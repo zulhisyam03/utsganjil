06978a70b8cc5c5c4ac574cae35b931b88df7919
zulhisyam03 <zul0342@gmail.com>
2022-03-21 00:30:23 +0800

    -

--- pengajaran/index.php
+++ pengajaran/index.php
@@ -4,30 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="app.css" />
+    <title>Pemrograman Web</title>    
 </head>
 <body>
     <div class="container">
         <header>
-            Judul
+            <h2>Website Pemrograman Web<br>STMIK ADHI GUNA PALU</h2>
         </header>
     
         <Div class="konten">
-            <div>Konten</div>            
+            <div>
+                <iframe src="input-pelanggan.html" frameborder="0" name="konten" height="500px"></iframe>    
+            </div>            
         </Div>
         <!--MENU-->  
-        <Div class="menu1">
-            <div>Menu 1</div> 
-        </Div>
-        <div class="menu2">
-            <div>Menu 2</div>
-        </div>
+        <a href="Tugas-2" class="menu1" target="konten">
+            <div class="menu"> TUGAS PHP </div> 
+        </a>
+        <a href="input-pelanggan.html" class="menu2" target="konten">
+            <div class="menu">Input Pelanggan</div>
+        </a>
         <div class="menu3">
-            <div>Menu 3</div>
+            <div class="menu">Menu 3</div>
         </div>
         <div class="menu4">
-            <div>Menu 4</div>
+            <div class="menu">Menu 4</div>
         </div>
         <!-- END MENU-->
     
